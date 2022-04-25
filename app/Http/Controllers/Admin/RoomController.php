eb4c6b9fa5fdc691a8a1d8a5c6972dc413b6f3da
vanlong long <vanlong11x1@gmail.com>
2022-04-25 15:53:37 +0700

    update name model

--- app/Http/Controllers/Admin/RoomController.php
+++ app/Http/Controllers/Admin/RoomController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Room_List;
+use App\Models\Room_list;
 use App\Http\Requests\RoomRequest;
 use Illuminate\Http\Request;
 
@@ -15,29 +15,29 @@ class RoomController extends Controller
     }
     public function addRoom(RoomRequest $request)
     {
-        $rooms = new Room_List;
+        $rooms = new Room_list;
         $rooms->fill($request->all())->save();
         return redirect()->route('room.index');
     }
     public function roomIndex()
     {
-        $rooms = Room_List::all();
+        $rooms = Room_list::all();
         return view('admin.room.index', compact('rooms'));
     }
     public function showEditRoom($id)
     {
-        $rooms = Room_List::find($id);
+        $rooms = Room_list::find($id);
         return view('admin.room.update', compact('rooms'));
     }
     public function updateRoom(RoomRequest $request, $id)
     {
-        $rooms = Room_List::find($id);
+        $rooms = Room_list::find($id);
         $rooms->fill($request->all())->update();
         return redirect()->route('room.index');
     }
     public function deleteRoom($id)
     {
-        $rooms = Room_List::find($id);
+        $rooms = Room_list::find($id);
         $rooms->delete();
         return redirect()->route('room.index');
     }
